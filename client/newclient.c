5a89ec6d757b33f648232a5481f36b4ca020d88b
remremi43 <151617423+remremi43@users.noreply.github.com>
2024-01-23 14:12:05 +0100

    Create newclient.c

---
+++ client/newclient.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <bluetooth/bluetooth.h>
+#include <bluetooth/l2cap.h>
+#include <assert.h>
+#include <time.h>
+#include <sys/time.h>
+#include <errno.h>
+
